9014b9eb3cabde8d419eecec3db555229c475d05
Sri Hari Krishna Narayanan <sriharikrishna@gmail.com>
2022-04-14 16:13:26 -0500

    Add Bude-openmp benchmarking

---
+++ openmp/bench/omp-single/res.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3.8
+import os
+
+def printfun(numthreads, blocklist,itercount,mode):
+     for s in blocklist:
+        os.system("echo -n {},{}, >> results.txt".format(numthreads,s))
+        os.system("grep  Average omp-single{}_{}_{}_{}.txt >> results.txt".format(mode,numthreads, itercount, s))
+     os.system("sed -i \"s/- Average time:   //g\" results.txt")
+     os.system("sed -i \"s/ ms//g\" results.txt")
+
+itercount=100
+os.system("rm results.txt")
+#Strong scaling
+for mode in ["-forward","-gradient"]:
+  for numthreads in [1,8,16,24,32,40,48,56,64]:
+    printfun(numthreads, [96], itercount,mode)
+
---
+++ openmp/bench/omp-single/script.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3.8
+import os
+
+def printfun(numthreads, blocklist,itercount):
+  for s in blocklist:
+      for mode in ["-forward","-gradient"]:
+        os.system("OMP_NUM_THREADS={}  taskset -c 0-{} numactl -i all ~/enzyme-sc22/BUDE/openmp/omp-single{}.exe -n {} -i {} > omp-single{}_{}_{}_{}.txt".format(numthreads, numthreads-1, mode, s*s*s, itercount, mode,numthreads, itercount, s))
+
+itercount=100
+#Strong scaling
+for numthreads in [1,8,16,24,32,40,48,56,64]:
+  printfun(numthreads, [96], itercount)
+
