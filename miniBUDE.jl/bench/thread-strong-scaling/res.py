5542479b173fee05c5a160f2569376c217f7d328
Sri Hari Krishna Narayanan <sriharikrishna@gmail.com>
2022-06-01 22:34:19 -0500

    Fix the scripts to use default poses

--- miniBUDE.jl/bench/thread-strong-scaling/res.py
+++ miniBUDE.jl/bench/thread-strong-scaling/res.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3.8
 import os
 
-def printfun(numthreads, blocklist,itercount,mode):
-     for s in blocklist:
-        os.system("echo -n {},{}, >> results.txt".format(numthreads,s))
-        os.system("grep  Average omp-single{}_{}_{}_{}.txt >> results.txt".format(mode,numthreads, itercount, s))
+def printfun(numthreads, itercount,mode):
+     os.system("echo -n {}, >> results.txt".format(numthreads))
+     os.system("grep  Average omp-single{}_{}_{}.txt >> results.txt".format(mode,numthreads, itercount))
      os.system("sed -i \"s/- Average time:   //g\" results.txt")
      os.system("sed -i \"s/ ms//g\" results.txt")
 
@@ -13,5 +12,4 @@ itercount=8
 #Strong scaling
 for mode in ["-false","-true"]:
   for numthreads in [1,8,16,24,32,40,48,56,64]:
-    printfun(numthreads, [96], itercount,mode)
-
+    printfun(numthreads, itercount,mode)
--- miniBUDE.jl/bench/thread-strong-scaling/script.py
+++ miniBUDE.jl/bench/thread-strong-scaling/script.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3.8
 import os
 
-def printfun(numthreads, blocklist,itercount):
-  for s in blocklist:
+def printfun(numthreads, itercount):
+      os.chdir("../../")
       for mode in ["false","true"]:
-        os.chdir("/home/ubuntu/enzyme-sc22/BUDE/miniBUDE.jl/")
-        os.system("julia --project=Threaded --threads={} src/Threaded.jl --enzyme {} --iterations {} --n {}  > omp-single-{}_{}_{}_{}.txt".format(numthreads,mode,itercount,s,mode,numthreads,itercount,s)) 
-        os.system(" mv *.txt /home/ubuntu/enzyme-sc22/BUDE/miniBUDE.jl/bench/thread-strong-scaling/")
+        os.system("julia --project=Threaded --threads={} src/Threaded.jl --enzyme {} --iterations {}  > omp-single-{}_{}_{}.txt".format(numthreads,mode,itercount,mode,numthreads,itercount))
+        os.system(" mv *.txt bench/thread-strong-scaling/")
+      os.chdir("bench/thread-strong-scaling")
 
 itercount=8
 #Strong scaling
 for numthreads in [1,8,16,24,32,40,48,56,64]:
-  printfun(numthreads, [96], itercount)
-
+  printfun(numthreads, itercount)
