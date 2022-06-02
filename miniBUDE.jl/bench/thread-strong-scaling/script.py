fba49e2b30ab527b82456e0531817a8f8c3f3db2
Sri Hari Krishna Narayanan <sriharikrishna@gmail.com>
2022-06-02 00:52:42 -0500

    Change scripts to relative paths

--- miniBUDE.jl/bench/thread-strong-scaling/script.py
+++ miniBUDE.jl/bench/thread-strong-scaling/script.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3.8
 import os
+import pathlib
+scriptdir = pathlib.Path(__file__).parent.resolve()
 
 def printfun(numthreads, itercount):
-      os.chdir("../../")
+      os.chdir(str(scriptdir)+"/../../")
       for mode in ["false","true"]:
         os.system("julia --project=Threaded --threads={} src/Threaded.jl --enzyme {} --iterations {}  > omp-single-{}_{}_{}.txt".format(numthreads,mode,itercount,mode,numthreads,itercount))
         os.system(" mv *.txt bench/thread-strong-scaling/")
-      os.chdir("bench/thread-strong-scaling")
+      os.chdir(scriptdir)
 
 itercount=8
 #Strong scaling
